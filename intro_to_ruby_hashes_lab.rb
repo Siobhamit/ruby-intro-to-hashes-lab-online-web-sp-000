c1ae2fd66f065c1adeb7c4402805c36e703c9b71
Siobhan The Destroyer <siobhancreagan@gmail.com>
2021-05-13 19:59:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,16 +3,23 @@ return new_hash = {}
 end
 
 def actor
-actor = {:name => "Dwwayne The Rock Johnson"}
+actor = {:name => "Dwayne The Rock Johnson"}
 end
 
 def monopoly
-	monopoly = {}
+	monopoly = {
+    :railroads => {}
+  }
 
 end
 
 def monopoly_with_second_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
+  monopoly = {
+    :railroads => {
+      :names => {}
+    }
+  }
+
 
 end
 
